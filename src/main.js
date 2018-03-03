a635193499a1a82b731048c9f389885450ee2b7d
alexy <i.asan825.mail@gmail.com>
2018-03-03 02:18:48 -0800

    google analytics implemented

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App'
@@ -14,6 +15,10 @@ import Icon from 'vue-awesome/components/Icon'
 
 import '@/assets/css/main.sass'
 
+Vue.use(VueAnalytics, {
+	id: 'UA-112511961-5',
+	router
+})
 Vue.use(VeeValidate)
 Vue.use(VInputmask)
 Vue.use(VueAxios, axios)
